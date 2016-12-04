fd9cc808bbf4e45edf11a40c8605cebebacf09de
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-12-04 09:04:24 -0800

    code cleanup and refactor

--- kubernetes.go
+++ kubernetes.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type NamespaceList struct {
 	Items []Namespace `json:"items"`
 }
@@ -35,4 +44,82 @@ type Metadata struct {
 	Name        string            `json:"name"`
 	Namespace   string            `json:"namespace"`
 	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+func getConfigMaps(namespace string) (*ConfigMapList, error) {
+	u := fmt.Sprintf("http://127.0.0.1:8001/api/v1/namespaces/%s/configmaps?labelSelector=konfd.io/template=true",
+		namespace)
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("non 200 response code")
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	var cl ConfigMapList
+	err = json.Unmarshal(data, &cl)
+	if err != nil {
+		return nil, err
+	}
+	return &cl, nil
+}
+
+func createConfigMap(namespace, name, key, value string) error {
+	c := ConfigMap{
+		ApiVersion: "v1",
+		Data:       make(map[string]string),
+		Kind:       "ConfigMap",
+		Metadata: Metadata{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	c.Data[key] = value
+
+	body, err := json.MarshalIndent(&c, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error encoding configmap %s: %v", name, err)
+	}
+
+	u := fmt.Sprintf("http://127.0.0.1:8001/api/v1/namespaces/%s/configmaps", namespace)
+	resp, err := http.Post(u, "", bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("error creating configmap %s: %v", name, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		return fmt.Errorf("error creating configmap %s; got HTTP %v status code", name, resp.StatusCode)
+	}
+
+	return nil
+}
+
+func getNamespaces() (*NamespaceList, error) {
+	resp, err := http.Get("http://127.0.0.1:8001/api/v1/namespaces")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("non 200 response code")
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	var nl NamespaceList
+	err = json.Unmarshal(data, &nl)
+	if err != nil {
+		return nil, err
+	}
+	return &nl, nil
 }
--- main.go
+++ main.go
@@ -2,64 +2,34 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
-	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("http://127.0.0.1:8001/api/v1/namespaces")
+	namespaces, err := getNamespaces()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if resp.StatusCode != 200 {
-		log.Fatal("non 200 response code")
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	resp.Body.Close()
 
-	var ns NamespaceList
-	err = json.Unmarshal(data, &ns)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, n := range ns.Items {
-		u := fmt.Sprintf("http://127.0.0.1:8001/api/v1/namespaces/%s/configmaps?labelSelector=konfd.io/template=true",
-			n.Metadata.Name)
-		resp, err := http.Get(u)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if resp.StatusCode != 200 {
-			log.Fatal("non 200 response code")
-		}
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp.Body.Close()
+	for _, namespace := range namespaces.Items {
+		namespaceName := namespace.Metadata.Name
 
-		var cs ConfigMapList
-		err = json.Unmarshal(data, &cs)
+		configmaps, err := getConfigMaps(namespaceName)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			continue
 		}
 
-		for _, cm := range cs.Items {
-			ts, ok := cm.Data["template"]
+		for _, configmap := range configmaps.Items {
+			ts, ok := configmap.Data["template"]
 			if !ok {
 				log.Println("missing template key")
 				continue
 			}
-			t := template.New(cm.Metadata.Name)
+			t := template.New(configmap.Metadata.Name)
 			tp := &TemplateProcessor{
-				namespace:  n.Metadata.Name,
+				namespace:  namespaceName,
 				configMaps: make(map[string]ConfigMap),
 				secrets:    make(map[string]Secret),
 				templates:  make(map[string]ConfigMap),
@@ -76,41 +46,20 @@ func main() {
 				continue
 			}
 
-			var buf bytes.Buffer
-			err = t.Execute(&buf, nil)
+			var value bytes.Buffer
+			err = t.Execute(&value, nil)
 			if err != nil {
 				log.Println("error executing template: %v", err)
 				continue
 			}
 
-			annotations := cm.Metadata.Annotations
-			cm2 := ConfigMap{
-				ApiVersion: "v1",
-				Data:       make(map[string]string),
-				Kind:       "ConfigMap",
-				Metadata: Metadata{
-					Name:      annotations["konfd.io/name"],
-					Namespace: n.Metadata.Name,
-				},
-			}
+			annotations := configmap.Metadata.Annotations
+			name := annotations["konfd.io/name"]
+			key := annotations["konfd.io/key"]
 
-			cm2.Data[annotations["konfd.io/key"]] = buf.String()
-			jsonEncoded, err := json.MarshalIndent(&cm2, "", "  ")
+			err = createConfigMap(namespaceName, name, key, value.String())
 			if err != nil {
-				log.Printf("error encoding object: %v", err)
-				continue
-			}
-
-			configmapURL := fmt.Sprintf("http://127.0.0.1:8001/api/v1/namespaces/%s/configmaps", n.Metadata.Name)
-
-			log.Println(string(jsonEncoded))
-			resp, err := http.Post(configmapURL, "", bytes.NewReader(jsonEncoded))
-			if err != nil {
-				log.Printf("error posting object: %v", err)
-				continue
-			}
-			if resp.StatusCode != 201 {
-				log.Printf("error posting object: non 200 error: %v", resp.StatusCode)
+				log.Println(err)
 				continue
 			}
 		}
